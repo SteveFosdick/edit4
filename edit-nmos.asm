SteveFosdick/edit4

commit d36be6f16651dea56631ca23c4bf822c52cc6aec

nmos: Fix two data as code cases and remove unused EQUs.

edit-nmos.asm
@@ -167,398 +167,51 @@ jF2 EQU &00F2
 jFE EQU &00FE
 jFF EQU &00FF
 
-
-brkv  EQU &0202
-
-j3438 EQU &3438
-j6341 EQU &6341
-j6568 EQU &6568
-j2070 EQU &2070
-j6570 EQU &6570
-j6873 EQU &6873
-j2044 EQU &2044
-j0D0A EQU &0D0A
-j5345 EQU &5345
-j6F63 EQU &6F63
-j6974 EQU &6974
-j6575 EQU &6575
-j7634 EQU &7634
-j4954 EQU &4954
-j04C8 EQU &04C8
+j020E EQU &020E
 j0400 EQU &0400
-j0464 EQU &0464
 j0463 EQU &0463
+j0464 EQU &0464
 j04C7 EQU &04C7
+j04C8 EQU &04C8
 j04FB EQU &04FB
-j181C EQU &181C
-j183C EQU &183C
-j1818 EQU &1818
-j0600 EQU &0600
-j616E EQU &616E
-j2065 EQU &2065
-j616F EQU &616F
-j20EA EQU &20EA
-j33B2 EQU &33B2
-j5C8F EQU &5C8F
-j04FF EQU &04FF
-j6962 EQU &6962
-j6F74 EQU &6F74
-j0500 EQU &0500
 j04FC EQU &04FC
 j04FD EQU &04FD
 j04FE EQU &04FE
-jD3B5 EQU &D3B5
-jFAA3 EQU &FAA3
-j6573 EQU &6573
-j52A9 EQU &52A9
-j6553 EQU &6553
-j2020 EQU &2020
-j6165 EQU &6165
-j4DA9 EQU &4DA9
-j43A9 EQU &43A9
-j646F EQU &646F
-j20A9 EQU &20A9
-j7261 EQU &7261
-j616D EQU &616D
-j6F4C EQU &6F4C
-j6153 EQU &6153
-j2064 EQU &2064
-j626F EQU &626F
-j696C EQU &696C
-j6CA9 EQU &6CA9
-j6966 EQU &6966
-j53A9 EQU &53A9
-j6361 EQU &6361
-j6C50 EQU &6C50
-j6574 EQU &6574
-j5320 EQU &5320
-j4520 EQU &4520
-j2087 EQU &2087
-j4320 EQU &4320
-j7570 EQU &7570
-j6556 EQU &6556
-j2E4E EQU &2E4E
-j6373 EQU &6373
-j7520 EQU &7520
-j4420 EQU &4420
-j7274 EQU &7274
-j203A EQU &203A
-j7473 EQU &7473
-j2073 EQU &2073
-j6320 EQU &6320
-j656C EQU &656C
-j7962 EQU &7962
-j4154 EQU &4154
-j6F77 EQU &6F77
-j2F6C EQU &2F6C
-j6564 EQU &6564
-j7465 EQU &7465
-j6874 EQU &6874
-j6261 EQU &6261
-j7320 EQU &7320
-j4F43 EQU &4F43
-j7270 EQU &7270
-j6F6E EQU &6F6E
-j6C61 EQU &6C61
-j6F73 EQU &6F73
-j656B EQU &656B
-j7563 EQU &7563
-j666F EQU &666F
-j4528 EQU &4528
-j6F6D EQU &6F6D
-j208A EQU &208A
-j6420 EQU &6420
-j6E65 EQU &6E65
-jEA20 EQU &EA20
-j6854 EQU &6854
-j6163 EQU &6163
-j6220 EQU &6220
-j766F EQU &766F
-j2061 EQU &2061
-j7765 EQU &7765
-j2E65 EQU &2E65
-j6F46 EQU &6F46
-j706F EQU &706F
-j6C20 EQU &6C20
-j4552 EQU &4552
-j2820 EQU &2820
-j2E29 EQU &2E29
-j73EA EQU &73EA
-j7365 EQU &7365
-j6120 EQU &6120
-j630D EQU &630D
-j2E79 EQU &2E79
-j6977 EQU &6977
-jEA0D EQU &EA0D
-j6F43 EQU &6F43
-j7364 EQU &7364
-j7973 EQU &7973
-j5420 EQU &5420
-j7369 EQU &7369
-j7461 EQU &7461
-j6E6F EQU &6E6F
-j7845 EQU &7845
-j6E75 EQU &6E75
-j6920 EQU &6920
-j7974 EQU &7974
-j6843 EQU &6843
-j6562 EQU &6562
-j4920 EQU &4920
-j6E61 EQU &6E61
-j764F EQU &764F
-j6E69 EQU &6E69
-j4F20 EQU &4F20
-j6C6F EQU &6C6F
-j2041 EQU &2041
-j6520 EQU &6520
-j520D EQU &520D
-j6D61 EQU &6D61
-j6661 EQU &6661
-j0D27 EQU &0D27
-j5220 EQU &5220
-j7420 EQU &7420
-j7375 EQU &7375
-j66EA EQU &66EA
-j6C41 EQU &6C41
-j6F20 EQU &6F20
-j6D27 EQU &6D27
-j206C EQU &206C
-j6F62 EQU &6F62
-j6168 EQU &6168
-j6572 EQU &6572
-j530D EQU &530D
-j6F54 EQU &6F54
-j08B0 EQU &08B0
-j7463 EQU &7463
-j662D EQU &662D
-j6F42 EQU &6F42
-j6552 EQU &6552
-j0D2E EQU &0D2E
-j7361 EQU &7361
-j3466 EQU &3466
-j6863 EQU &6863
-j2023 EQU &2023
-j2420 EQU &2420
-j202C EQU &202C
-j2C79 EQU &2C79
-j205B EQU &205B
-j207A EQU &207A
-j0D7A EQU &0D7A
-j6E20 EQU &6E20
-j2A20 EQU &2A20
-j205E EQU &205E
-j205C EQU &205C
-j7469 EQU &7469
-j6E4F EQU &6E4F
-j6957 EQU &6957
-j7327 EQU &7327
-j6665 EQU &6665
-j4720 EQU &4720
-j4120 EQU &4120
-j2C6C EQU &2C6C
-j726F EQU &726F
-j3566 EQU &3566
-j7220 EQU &7220
-j202F EQU &202F
-j203B EQU &203B
-j6E25 EQU &6E25
-j6F66 EQU &6F66
-j6D20 EQU &6D20
-j7961 EQU &7961
-j6544 EQU &6544
-j654B EQU &654B
-j4B28 EQU &4B28
-j440D EQU &440D
-j204B EQU &204B
-j6F70 EQU &6F70
-j5F28 EQU &5F28
-j6D65 EQU &6D65
-j540D EQU &540D
-j6D75 EQU &6D75
-j6964 EQU &6964
-j6F68 EQU &6F68
-j2079 EQU &2079
-jFF28 EQU &FF28
-j2067 EQU &2067
-j7020 EQU &7020
-j0D0D EQU &0D0D
-j6C70 EQU &6C70
-j544F EQU &544F
-j6C63 EQU &6C63
-j756F EQU &756F
-j2D74 EQU &2D74
-j6D69 EQU &6D69
-j6465 EQU &6465
-j6554 EQU &6554
-j6228 EQU &6228
-j550D EQU &550D
-j7774 EQU &7774
-j0D61 EQU &0D61
-j6620 EQU &6620
-j6F7A EQU &6F7A
-j6965 EQU &6965
-j7543 EQU &7543
-j410D EQU &410D
-j7865 EQU &7865
-j0732 EQU &0732
-j6445 EQU &6445
-j4CEA EQU &4CEA
-j0733 EQU &0733
-j776F EQU &776F
-j60EA EQU &60EA
-j7B20 EQU &7B20
-j7620 EQU &7620
-j206E EQU &206E
-j2072 EQU &2072
-j612E EQU &612E
-j6E72 EQU &6E72
-j622E EQU &622E
-j7062 EQU &7062
-j6170 EQU &6170
-j6363 EQU &6363
-j2063 EQU &2063
-j0D7D EQU &0D7D
-j6563 EQU &6563
-j0D65 EQU &0D65
-j656D EQU &656D
-j2074 EQU &2074
-j6D64 EQU &6D64
-j652E EQU &652E
-j2E0D EQU &2E0D
-j2E20 EQU &2E20
-j7665 EQU &7665
-j6666 EQU &6666
-j7720 EQU &7720
-j6669 EQU &6669
-j736F EQU &736F
-j6769 EQU &6769
-j307B EQU &307B
-j692E EQU &692E
-j6A20 EQU &6A20
-j6F7B EQU &6F7B
-j6C6C EQU &6C6C
-j6475 EQU &6475
-j377B EQU &377B
-j6C2E EQU &6C2E
-j7073 EQU &7073
-j296E EQU &296E
-j6E2E EQU &6E2E
-j656E EQU &656E
-j206A EQU &206A
-j756A EQU &756A
-j702E EQU &702E
-j2D2B EQU &2D2B
-j7467 EQU &7467
-j357B EQU &357B
-j722E EQU &722E
-j6C66 EQU &6C66
-j732E EQU &732E
-j742E EQU &742E
-j387B EQU &387B
-j3631 EQU &3631
-j3432 EQU &3432
-j2E2E EQU &2E2E
-j392C EQU &392C
-j4A20 EQU &4A20
-j752E EQU &752E
-j756E EQU &756E
-j3020 EQU &3020
-j7053 EQU &7053
-j202E EQU &202E
-j2C6E EQU &2C6E
-j6E2B EQU &6E2B
-j6E2D EQU &6E2D
-j322B EQU &322B
-j2032 EQU &2032
-j460D EQU &460D
-j6877 EQU &6877
-j0D64 EQU &0D64
-j7562 EQU &7562
-j6265 EQU &6265
-j7565 EQU &7565
-j636F EQU &636F
-j4843 EQU &4843
-j2029 EQU &2029
-j2031 EQU &2031
-j6F2E EQU &6F2E
-j7374 EQU &7374
-j4853 EQU &4853
-j2C29 EQU &2C29
-j2850 EQU &2850
-j2848 EQU &2848
-jEA3F EQU &EA3F
-j6F75 EQU &6F75
-j060A EQU &060A
+j04FF EQU &04FF
+j0500 EQU &0500
+j0501 EQU &0501
 j0502 EQU &0502
+j0503 EQU &0503
 j0514 EQU &0514
-j0516 EQU &0516
 j0515 EQU &0515
+j0516 EQU &0516
 j0517 EQU &0517
 j0518 EQU &0518
-j051A EQU &051A
 j0519 EQU &0519
+j051A EQU &051A
 j051B EQU &051B
 j052F EQU &052F
-j6F64 EQU &6F64
-j2C65 EQU &2C65
-j0501 EQU &0501
 j0530 EQU &0530
 j0531 EQU &0531
-j0503 EQU &0503
-j2017 EQU &2017
-j020E EQU &020E
-j4C58 EQU &4C58
-j5820 EQU &5820
-j4443 EQU &4443
-j4D44 EQU &4D44
-j69A8 EQU &69A8
-jD36C EQU &D36C
-jDB73 EQU &DB73
-jE376 EQU &E376
-jF465 EQU &F465
-j016A EQU &016A
-j72A9 EQU &72A9
-j74A9 EQU &74A9
-j75A9 EQU &75A9
-j632E EQU &632E
-j3072 EQU &3072
-j662E EQU &662E
-jEA0B EQU &EA0B
-j6968 EQU &6968
-j6F6C EQU &6F6C
-j6173 EQU &6173
-j5241 EQU &5241
-j4F54 EQU &4F54
-j5543 EQU &5543
-jF420 EQU &F420
-jEA1A EQU &EA1A
-j796C EQU &796C
-j2C30 EQU &2C30
-j2C34 EQU &2C34
-j6F4D EQU &6F4D
-j206F EQU &206F
-j0200 EQU &0200
-j6720 EQU &6720
-j746F EQU &746F
-j0728 EQU &0728
-j071E EQU &071E
-j6C75 EQU &6C75
-j746E EQU &746E
+j05FE EQU &05FE
+j05FF EQU &05FF
+j0600 EQU &0600
+j0601 EQU &0601
+j060A EQU &060A
+j0700 EQU &0700
 j0706 EQU &0706
 j070C EQU &070C
 j0712 EQU &0712
 j0718 EQU &0718
-j0700 EQU &0700
-j05FF EQU &05FF
-j05FE EQU &05FE
-j0601 EQU &0601
-j6000 EQU &6000
-jEA3A EQU &EA3A
-j20EF EQU &20EF
-j6F4E EQU &6F4E
-j7000 EQU &7000
+j071E EQU &071E
+j0728 EQU &0728
+j0732 EQU &0732
+j0733 EQU &0733
+jEA0B EQU &EA0B
 
- ORG &8000
+brkv  EQU &0202
+
+    ORG &8000
 
 ;==============================================================================
 ;=
@@ -3624,15 +3277,7 @@ jAB16
  CPX #&09
  BNE jAB16
  JSR vstrng
- JSR j2017
- BRK
- BRK
- BRK
- BRK
- BRK
- BRK
- BRK
- BRK
+ DFB &20,&17,&20,&00,&00,&00,&00,&00,&00,&00,&00
  NOP
  LDX markCnt
  LDY j4E
@@ -5894,7 +5539,8 @@ jBC7F
  JSR jAEC2
  LDX #&0E
  JSR j98EF
- JSR j20EA
+ DFB ' '
+ NOP
  LDA (jBC,X)
  JSR j98EF
  DATA " found"

commit b9cca21e01f29191849474ea007d387ee9bc486f

Fix error introduced in d36be6f16651dea56631ca23c4bf822c52cc6aec

edit-nmos.asm
@@ -5541,7 +5541,7 @@ jBC7F
  JSR j98EF
  DFB ' '
  NOP
- LDA (jBC,X)
+ JSR jBCA1
  JSR j98EF
  DATA " found"
  NOP